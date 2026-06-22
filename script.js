f7484f8fea05d82924fd90906c6de2ae6fba47ac
Юрій <85017533+Mikuz1@users.noreply.github.com>
2026-06-22 19:29:35 +0300

    Change about link from 'about.html' to '#about'

--- script.js
+++ script.js
@@ -547,7 +547,7 @@ document.addEventListener('DOMContentLoaded', function () {
         { href: 'index.html', element: document.querySelector('.hero') },
         { href: '#music', element: document.getElementById('music') },
         { href: '#video', element: document.getElementById('video') },
-        { href: 'about.html', element: document.getElementById('about') },
+        { href: '#about', element: document.getElementById('about') },
         { href: '#service', element: document.getElementById('service') },
         { href: '#contact', element: document.getElementById('contact') },
     ].filter(item => item.element && headerNavLinks.some(link => link.getAttribute('href') === item.href));
